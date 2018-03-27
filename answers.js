1ac8f650c39df1cabea8fb9aa4399c0f175c0286
Jason Spiller <jason@thetargetlab.com>
2018-03-26 20:45:07 -0600

    1st commit - selecting datatypes

---
+++ answers.js
@@ -0,0 +1,20 @@
+//
+// Jason Spiller
+// 03.26.18
+// https://github.com/jasonspiller/week-02-day-1-datatypes-objects.git
+// Description:
+// Answers to WDI7 week 2 day 1 homework.
+//
+
+// 1. A light switch that can be either on or off.
+// Boolean
+// 2. A user's email address.
+// String
+// 3. A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// Object
+// 4. A list of student names from our class.
+// Array
+// 5. A list of student names from our class, each with a location.
+// Array of Objects
+// 6. A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// Array of objects
